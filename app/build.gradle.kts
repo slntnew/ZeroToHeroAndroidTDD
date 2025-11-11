ff8eee72ca8543e9ced2d78f63278a84b60fec03
criterionsz <salimagahramanova@gmail.com>
2025-11-11 12:56:50 +0900

    Task001Test solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,11 @@ plugins {
 
 android {
     namespace = "ru.easycode.zerotoheroandroidtdd"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.easycode.zerotoheroandroidtdd"
         minSdk = 21
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,11 +36,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
