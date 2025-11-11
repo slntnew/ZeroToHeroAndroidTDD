51eaa8d9f583388ed6fb134f01097881da395e2e
criterionsz <salimagahramanova@gmail.com>
2025-11-11 14:09:38 +0900

    Task007Test solved (freeze and restore solutions)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,18 +2,35 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.os.bundleOf
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var text: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.changeButton)
-        val text = findViewById<TextView>(R.id.titleTextView)
+        text = findViewById<TextView>(R.id.titleTextView)
         button.setOnClickListener {
             text.text = "I am an Android Developer!"
         }
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", text.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        text.text = savedInstanceState.getString("key")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
