735aed25176392c948f675740b827dd6b056a642
criterionsz <salimagahramanova@gmail.com>
2025-11-11 23:43:17 +0900

    Task011Test solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,19 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import kotlin.IllegalStateException
+
+interface Count {
+
+    fun increment(number: String): String
+    class Base(private val step: Int) : Count {
+        init {
+            if (step < 1) {
+                throw IllegalStateException("step should be positive, but was -2")
+            }
+        }
+
+        override fun increment(number: String): String {
+           return (number.toInt()+step).toString()
+        }
+    }
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  countTextView: TextView
+    private lateinit var incrementButton: Button
+    private var count: Count = Count.Base(step = 2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        countTextView = findViewById(R.id.countTextView)
+        incrementButton = findViewById(R.id.incrementButton)
+        incrementButton.setOnClickListener {
+            countTextView.text = count.increment(countTextView.text.toString()).toString()
+        }
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    companion object {
+        private const val COUNT_KEY = "count"
+        private const val ADD = 2
     }
 }
